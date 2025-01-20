5e11f5764d0bf1cb3436cb10a3b3cdf811ef39bf
subroto18 <subroto@revivotech.in>
2025-01-20 20:35:47 +0530

    implement becrypt

--- models/user.model.js
+++ models/user.model.js
@@ -30,11 +30,40 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async (next) => {
+  let user = this;
+  if (!user.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(user.password, salt);
+  user.password = hashedPassword;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (userPassword) {
+  try {
+    const isPasswordMatch = await bcrypt.compare(userPassword, this.password);
+    return isPasswordMatch;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
